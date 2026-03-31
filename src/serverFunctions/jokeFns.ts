c36914852bf25b2f2808a81b209181df11abfbad
Anna Moughnie <anna@moughnie.com>
2026-03-30 22:42:21 -0700

    finished user functions with betterauth

--- src/serverFunctions/jokeFns.ts
+++ src/serverFunctions/jokeFns.ts
@@ -1,26 +1,27 @@
 import { createServerFn } from "@tanstack/react-start";
 import type { CreateJokeInput, DeleteJokeInput, VoteJokeInput } from "#/types";
+import { requireSession } from "./userFns";
 
 export const getJokes = createServerFn({ method: "GET" }).handler(
-  async ({ context }) => {
-    return context.jokeService.getJokes();
-  },
+    async ({ context }) => {
+        return context.jokeService.getJokes();
+    },
 );
 
 export const createJoke = createServerFn({ method: "POST" })
-  .inputValidator((input: CreateJokeInput) => input)
-  .handler(async ({ data, context }) => {
-    return context.jokeService.createJoke(data);
-  });
+    .inputValidator((input: CreateJokeInput) => input)
+    .handler(async ({ data, context }) => {
+        return context.jokeService.createJoke(data);
+    });
 
 export const voteJoke = createServerFn({ method: "POST" })
-  .inputValidator((input: VoteJokeInput) => input)
-  .handler(async ({ data, context }) => {
-    return context.jokeService.voteJoke(data);
-  });
+    .inputValidator((input: VoteJokeInput) => input)
+    .handler(async ({ data, context }) => {
+        return context.jokeService.voteJoke(data);
+    });
 
 export const deleteJoke = createServerFn({ method: "POST" })
-  .inputValidator((input: DeleteJokeInput) => input)
-  .handler(async ({ data, context }) => {
-    return context.jokeService.deleteJoke(data);
-  });
+    .inputValidator((input: DeleteJokeInput) => input)
+    .handler(async ({ data, context }) => {
+        return context.jokeService.deleteJoke(data);
+    });
--- src/serverFunctions/userFns.ts
+++ src/serverFunctions/userFns.ts
@@ -1,8 +1,60 @@
 import { createServerFn } from "@tanstack/react-start";
+import { getRequestHeaders } from "@tanstack/react-start/server";
 import type { CreateUserInput } from "#/types";
+import { auth } from "#/auth/auth";
 
-export const creaetUser = createServerFn({ method: "POST" })
+export const createUser = createServerFn({ method: "POST" })
     .inputValidator((input: CreateUserInput) => input)
-    .handler(async ({ data, context }) => {
-        return context.userService.createUser(data);
+    .handler(async ({ data }) => {
+        return await auth.api.signUpEmail({
+            body: {
+                name: data.fullname,
+                email: data.email,
+                password: data.password,
+            },
+        });
     });
+
+export const signInUser = createServerFn({ method: "POST" })
+    .inputValidator((input: { email: string; password: string }) => input)
+    .handler(async ({ data }) => {
+        return await auth.api.signInEmail({
+            body: {
+                email: data.email,
+                password: data.password,
+            },
+        });
+    });
+
+export const signOutUser = createServerFn({ method: "POST" }).handler(
+    async () => {
+        const headers = getRequestHeaders();
+
+        return await auth.api.signOut({ headers });
+    },
+);
+
+export const getSession = createServerFn({ method: "GET" }).handler(
+    async () => {
+        const headers = getRequestHeaders();
+
+        return await auth.api.getSession({
+            headers,
+        });
+    },
+);
+
+export const requireSession = createServerFn({ method: "GET" }).handler(
+    async () => {
+        const headers = getRequestHeaders();
+
+        const session = await auth.api.getSession({ headers });
+
+        if (!session) {
+            // this is temporary I'm not sure if I have to do some proper error handling with this yet
+            throw new Error("unauthorized");
+        }
+
+        return session;
+    },
+);
